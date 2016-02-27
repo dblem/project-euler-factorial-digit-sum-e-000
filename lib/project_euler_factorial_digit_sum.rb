47e7648e9e5ea5f0913b3dff5fba9ce0645f883f
dblem <dax.blem@gmail.com>
2016-02-27 20:25:13 +0000

    Done.

--- lib/project_euler_factorial_digit_sum.rb
+++ lib/project_euler_factorial_digit_sum.rb
@@ -1,1 +1,23 @@
-# code your solution here
+def factorial(x)
+  product = 1
+  until x == 0
+    product *= x
+    x -= 1
+  end
+  product
+end
+
+def sum_of_digits(x)
+  num = x.to_s
+  sum = 0
+  digit_array = num.split("")
+  digit_array.each do |y|
+    sum += y.to_i
+  end
+  sum
+end
+
+def factorial_digit_sum(x)
+  factorial = factorial(x)
+  sum_of_digits(factorial)
+end
